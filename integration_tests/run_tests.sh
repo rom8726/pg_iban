de75788c838cea2d50318a00fa1aa1625d0a8b2f
Roman Chudov <roman.chudov@gmail.com>
2025-05-06 23:22:44 +0300

    Small improvement.

--- integration_tests/run_tests.sh
+++ integration_tests/run_tests.sh
@@ -2,7 +2,7 @@
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
-NC='\033[0m' # No Color
+NC='\033[0m'
 
 initdb -D /var/lib/postgresql/16/data
 
@@ -12,8 +12,10 @@ sleep 5
 psql --set ON_ERROR_STOP=on -U $POSTGRES_USER -d postgres -f /opt/pg_iban/integration_tests/01_create_database.sql
 TEST_RESULT=$?
 if [ $TEST_RESULT -eq 0 ]; then
+    echo -e "\n\n=== Starting Integration Tests ===\n\n"
     psql --set ON_ERROR_STOP=on -U $POSTGRES_USER -d test_db -f /opt/pg_iban/integration_tests/02_integration_tests.sql
     TEST_RESULT=$?
+    echo -e "\n\n=== Finished Integration Tests ===\n\n"
 fi
 
 pg_ctl stop -D /var/lib/postgresql/16/data
