559e6e7abb66b083e119c82a8c6e8d6bcd9f5b7a
Roman Chudov <roman.chudov@gmail.com>
2025-05-06 08:29:06 +0300

    tests fixed.

--- test_pg_iban.c
+++ test_pg_iban.c
@@ -6,7 +6,6 @@
 
 static void test_valid_iban(void **state) {
     assert_true(validate_pg_iban("DE89370400440532013000"));
-    assert_true(validate_pg_iban("FR7612345678901234567890123"));
     assert_true(validate_pg_iban("GB82WEST12345698765432"));
 }
 
@@ -14,6 +13,7 @@ static void test_invalid_iban(void **state) {
     assert_false(validate_pg_iban("INVALIDIBAN"));
     assert_false(validate_pg_iban("12345"));
     assert_false(validate_pg_iban("DE8937040044XX32013000"));
+    assert_false(validate_pg_iban("FR7612345678901234567890123"));
 }
 
 static void test_empty_iban(void **state) {
