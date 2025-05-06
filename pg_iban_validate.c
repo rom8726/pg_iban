ba570117877ed5514718c40615202ef26c8e54eb
Roman Chudov <roman.chudov@gmail.com>
2025-05-06 07:36:32 +0300

    validate function refactored

--- pg_iban_validate.c
+++ pg_iban_validate.c
@@ -4,37 +4,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+static int char_to_num(char c) {
+    if (isdigit(c)) return c - '0';
+    if (isupper(c)) return c - 'A' + 10;
+    if (islower(c)) return c - 'a' + 10;
+    return -1;
+}
+
+static bool validate_iban_checksum(const char *iban_clean) {
+    char temp[256];
+    int len = strlen(iban_clean);
+
+    strncpy(temp, iban_clean + 4, len - 4);
+    strncpy(temp + len - 4, iban_clean, 4);
+    temp[len] = '\0';
+
+    char numeric[256];
+    for (int i = 0; i < len; i++) {
+        int num = char_to_num(temp[i]);
+        if (num < 0) return false;
+        sprintf(numeric + i * 2, "%02d", num);
+    }
+
+    unsigned long long mod = 0;
+    for (int i = 0; i < strlen(numeric); i++) {
+        mod = (mod * 10 + (numeric[i] - '0')) % 97;
+    }
+
+    return (mod == 1);
+}
+
 bool validate_pg_iban(const char *iban)
 {
     int len = strlen(iban);
     if (len < MIN_IBAN_LENGTH || len > MAX_IBAN_LENGTH)
         return false;
 
-    char rearranged[MAX_IBAN_LENGTH * 2 + 1];
-    snprintf(rearranged, sizeof(rearranged), "%s%s", iban + 4, iban);
-
-    char digits[MAX_IBAN_LENGTH * 4 + 1];
-    int pos = 0;
-    for (int i = 0; rearranged[i] != '\0'; i++) {
-        if (isdigit((unsigned char)rearranged[i])) {
-            digits[pos++] = rearranged[i];
-        } else if (isupper((unsigned char)rearranged[i])) {
-            int val = rearranged[i] - 'A' + 10;
-            pos += snprintf(digits + pos, sizeof(digits) - pos, "%d", val);
-        } else {
-            return false;
-        }
+    if (!isalpha(iban[0]) || !isalpha(iban[1])) {
+        return false;
     }
-    digits[pos] = '\0';
 
-    const char *p = digits;
-    long long rem = 0;
-    while (*p) {
-        char chunk[20];
-        int n = snprintf(chunk, sizeof(chunk), "%lld%.9s", rem, p);
-        chunk[n] = '\0';
-        rem = strtoll(chunk, NULL, 10) % 97;
-        p += 9;
+    if (!isdigit(iban[2]) || !isdigit(iban[3])) {
+        return false;
     }
-    return rem == 1;
+
+    if (!validate_iban_checksum(iban)) {
+        return false;
+    }
+
+    return true;
 }
